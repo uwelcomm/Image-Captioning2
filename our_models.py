121d3b387559066435764249dfbbdf08c04ef564
Mahmoud Qaid <mmaqaid@gmail.com>
2023-06-04 23:23:30 +0300

    push our_trainer.py, our_models.py,args.json,config.json

---
+++ our_models.py
@@ -0,0 +1,54 @@
+
+from models.blip import blip_decoder
+import pytorch_lightning as pl
+import json
+
+class TrainingModel(pl.LightningModule):
+    def __init__(self,config=None):
+        super().__init__()
+        if config==None:
+            config=json.load(open('config.json','r'))
+        self.model = blip_decoder(pretrained=config['pretrained'], image_size=384, vit='base')
+#         self.model = blip_decoder(pretrained=config['pretrained'], image_size=384, vit='base')
+        self.result = []
+
+        self.config = config
+        for param in self.model.visual_encoder.parameters():
+            param.requires_grad=config['train_vit']
+            
+
+    def training_step(self, batch, batch_idx):
+        image, caption,_= batch
+        loss = self.model(image, caption)      
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(params=self.model.parameters(), lr=self.config['init_lr'], weight_decay=self.config['weight_decay'])
+        return optimizer
+    
+    def validation_step(self, batch, batch_idx):
+        image, image_id= batch
+        captions = self.model.generate(image, sample=False, num_beams=self.config['num_beams'], max_length=self.config['max_length'], 
+                                  min_length=self.config['min_length'])
+        for caption, img_id in zip(captions, image_id):
+#             result.append({"image_id": img_id.item(), "caption": caption})
+            self.result.append({"image_id": img_id, "caption": caption})
+    def validation_epoch_end(self,outputs):
+        loss=0
+        if len(self.result)>=len(self.trainer.val_dataloaders[0])*self.config['batch_size']-self.config['batch_size']:
+            val_result_file = save_result(self.result, 'output/Caption_coco/result', 'val_epoch%d'%self.trainer.current_epoch , remove_duplicate='image_id')
+
+
+            coco_val=coco_caption_eval(self.config['ann_root'],val_result_file,'val')
+            loss=coco_val.eval['CIDEr'] + coco_val.eval['Bleu_4']
+            self.log_dict({'val_loss': loss,'Bleu_1':coco_val.eval['Bleu_1'],
+                                     'Bleu_2':coco_val.eval['Bleu_2'],
+                                     'Bleu_3':coco_val.eval['Bleu_3'],
+                                     'Bleu_4':coco_val.eval['Bleu_4'],
+                                     'CIDEr':coco_val.eval['CIDEr'],
+                                     'ROUGE_L':coco_val.eval['ROUGE_L'],
+                                     'METEOR':coco_val.eval['METEOR']})
+
+
+        self.result=[]
+        return {"val_loss": loss}
---
+++ our_trainer.py
@@ -0,0 +1,120 @@
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import TQDMProgressBar
+from lightning.pytorch.loggers import CSVLogger
+
+import argparse
+import os
+import pandas as pd
+import ruamel.yaml as yaml
+import numpy as np
+import random
+import time
+import datetime
+import json
+from pathlib import Path
+from tqdm.notebook import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+from torch.utils.data import DataLoader
+
+import utils
+from utils import cosine_lr_schedule
+from data import create_dataset, create_sampler, create_loader
+from data.utils import save_result, coco_caption_eval
+from our_models import TrainingModel
+
+class Args(dict):
+    __setattr__ = dict.__setitem__
+    __getattr__ = dict.__getitem__
+
+args = json.load(open('args.json','r'))
+config=json.load(open('config.json','r'))
+args = Args(args) # dict2object
+
+    
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+print("Creating captioning dataset")
+train_dataset, val_dataset, test_dataset = create_dataset('caption_coco', config)  
+
+
+samplers = [None, None, None]
+
+train_loader, val_loader, test_loader = create_loader([train_dataset, val_dataset, test_dataset],samplers,
+                                                      batch_size=[config['batch_size']]*3,num_workers=[4,4,4],
+                                                      is_trains=[True, False, False], collate_fns=[None,None,None])         
+
+
+if __name__=='__main__':
+    # %%time
+    args.result_dir = os.path.join(args.output_dir, 'result')
+    #     print(config)
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    Path(args.result_dir).mkdir(parents=True, exist_ok=True)
+    Path(config['ann_root']).mkdir(parents=True, exist_ok=True)
+    Path('logs').mkdir(parents=True, exist_ok=True)
+    
+#     if config['load_checkpoint']:
+# #         train_model = TrainingModel.load_from_checkpoint(config['checkpoint'],config=config,hparams_file=config['hparams_path'])
+#         train_model = TrainingModel.load_from_checkpoint(config['checkpoint'],config=config)
+# #         train_model = TrainingModel(config)
+#     else:
+#         train_model = TrainingModel(config)
+# #     train_model = TrainingModel.load_from_checkpoint('checkpoints/model-epoch=04-val_loss=0.03.ckpt',config=config)
+
+    train_model = TrainingModel(config)
+
+    # training
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath='checkpoints',
+        filename='model-{epoch:02d}-{val_loss:.2f}',
+
+        save_top_k=1,
+        monitor='val_loss',
+        mode='max'
+    )
+    logger=CSVLogger(save_dir="logs/")
+    
+
+    trainer = pl.Trainer(
+        default_root_dir="checkpoints/",
+    #     resume_from_checkpoint='checkpoints/model-epoch=00-val_loss=1.65.ckpt',
+
+        accelerator='auto', 
+        devices='auto', 
+#         precision=16,
+        callbacks=[checkpoint_callback,TQDMProgressBar(refresh_rate=10)],
+        max_epochs=config['max_epoch'], 
+        logger=logger,
+        strategy='auto'
+    )
+    print("Start training")
+#     trainer.fit(train_model, train_loader,val_loader,ckpt_path='checkpoints/model-epoch=00-val_loss=0.00.ckpt')
+    if args.evaluate:
+        if config['load_checkpoint'] and config['checkpoint']:
+            trainer.validate(train_model, val_loader,ckpt_path=config['checkpoint'])
+        else:
+            trainer.validate(train_model, val_loader)
+#             trainer.validate(train_model, test_loader)
+
+    else:
+        if config['load_checkpoint'] and config['checkpoint']:
+            trainer.fit(train_model, train_loader,val_loader,ckpt_path=config['checkpoint'])
+        else:
+            trainer.fit(train_model, train_loader,val_loader)
+        trainer.save_checkpoint(filepath="checkpoints/current_checkpoint.ckpt")
+
+#     trainer.fit(train_model, train_loader,val_loader)
+#     trainer.save_checkpoint(filepath="checkpoint.ckpt")
+
