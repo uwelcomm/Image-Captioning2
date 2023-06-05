5ac9d6bcd47ea848376b628d3993d156426a78b7
Mahmoud Qaid <mmaqaid@gmail.com>
2023-06-05 19:20:34 +0300

    add numbeams for our_predict

--- our_predict.py
+++ our_predict.py
@@ -1,8 +1,8 @@
-
+from models.blip import blip_decoder
 from PIL import Image
-
+import json
 from torchvision import transforms
-from our_models import TrainingModel
+# from our_models import TrainingModel
 
 normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
 
@@ -11,12 +11,11 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     normalize,
 ])     
-# model = TrainingModel.load_from_checkpoint('/kaggle/input/captioning-trainer-ckpt/Image-Captioning/checkpoints/best-model.ckpt',config=config)
-# model = TrainingModel(config=config)
-model = TrainingModel()
-model.eval()
 
-config=model.config
+config=json.load(open('config.json','r'))
+captioner=blip_decoder(pretrained=config['checkpoint']+'captioner_ckpt/captioner.pth', image_size=384, vit='base')
+captioner.eval()
+
 # !pip install happytransformer
 from happytransformer import HappyTextToText, TTSettings
 happy_tt = HappyTextToText("T5", config['checkpoint']+'corrector_ckpt')
@@ -24,12 +23,12 @@ happy_tt = HappyTextToText("T5", config['checkpoint']+'corrector_ckpt')
 
 args = TTSettings(num_beams=5, min_length=1)
 
-def predict(img_path):
+def predict(img_path,num_beams=config['num_beams']):
     image = Image.open(img_path).convert('RGB') 
     image = transform(image)
     image=image.unsqueeze(dim=0)
-    model.eval()
-    visual_caption=model.model.generate(image, sample=False, num_beams=config['num_beams'], max_length=config['max_length'], 
+    captioner.eval()
+    visual_caption=captioner.generate(image, sample=False, num_beams=num_beams, max_length=config['max_length'], 
                               min_length=config['min_length'])[0]
     result = happy_tt.generate_text("grammar: "+visual_caption, args=args)
     return result.text
