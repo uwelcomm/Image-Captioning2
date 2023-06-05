a8debecf97d0a0a0978b146e547e8c9bc3281a73
Mahmoud Qaid <mmaqaid@gmail.com>
2023-06-05 16:37:55 +0300

    remove unused data files

--- data/__init__.py
+++ data/__init__.py
@@ -4,11 +4,11 @@ from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
 
 from data.coco_karpathy_dataset import coco_karpathy_train, coco_karpathy_caption_eval
-from data.nocaps_dataset import nocaps_eval
+# from data.nocaps_dataset import nocaps_eval
 #from data.flickr30k_dataset import flickr30k_train, flickr30k_retrieval_eval
 #from data.vqa_dataset import vqa_dataset
 #from data.nlvr_dataset import nlvr_dataset
-from data.pretrain_dataset import pretrain_dataset
+# from data.pretrain_dataset import pretrain_dataset
 from transform.randaugment import RandomAugment
 
 def create_dataset(dataset, config, min_scale=0.5,annot_dict={'train':None,'val':None,'test':None}):
@@ -29,34 +29,11 @@ def create_dataset(dataset, config, min_scale=0.5,annot_dict={'train':None,'val'
         normalize,
         ])  
         
-    if dataset=='pretrain':
-        dataset = pretrain_dataset(config['train_file'], config['laion_path'], transform_train)              
-        return dataset  
-    
-    elif dataset=='caption_coco':   
-        train_dataset = coco_karpathy_train(transform_train, config['image_root'], config['ann_root'], prompt=config['prompt'],annot_list=annot_dict['train'])
-        val_dataset = coco_karpathy_caption_eval(transform_test, config['image_root'], config['ann_root'], 'val',annot_list=annot_dict['val'])
-        test_dataset = coco_karpathy_caption_eval(transform_test, config['image_root'], config['ann_root'], 'test',annot_list=annot_dict['test'])   
-        return train_dataset, val_dataset, test_dataset
-    
-    elif dataset=='nocaps':   
-        val_dataset = nocaps_eval(transform_test, config['image_root'], config['ann_root'], 'val')
-        test_dataset = nocaps_eval(transform_test, config['image_root'], config['ann_root'], 'test')   
-        return val_dataset, test_dataset   
-    
-    elif dataset=='vqa': 
-        train_dataset = vqa_dataset(transform_train, config['ann_root'], config['vqa_root'], config['vg_root'], 
-                                    train_files = config['train_files'], split='train') 
-        test_dataset = vqa_dataset(transform_test, config['ann_root'], config['vqa_root'], config['vg_root'], split='test')
-        return train_dataset, test_dataset
-    
-    elif dataset=='nlvr': 
-        train_dataset = nlvr_dataset(transform_train, config['image_root'], config['ann_root'],'train')
-        val_dataset = nlvr_dataset(transform_test, config['image_root'], config['ann_root'],'val')
-        test_dataset = nlvr_dataset(transform_test, config['image_root'], config['ann_root'],'test')     
-        return train_dataset, val_dataset, test_dataset   
-    
-    
+    train_dataset = coco_karpathy_train(transform_train, config['image_root'], config['ann_root'], prompt=config['prompt'],annot_list=annot_dict['train'])
+    val_dataset = coco_karpathy_caption_eval(transform_test, config['image_root'], config['ann_root'], 'val',annot_list=annot_dict['val'])
+    test_dataset = coco_karpathy_caption_eval(transform_test, config['image_root'], config['ann_root'], 'test',annot_list=annot_dict['test'])   
+    return train_dataset, val_dataset, test_dataset
+
 def create_sampler(datasets, shuffles, num_tasks, global_rank):
     samplers = []
     for dataset,shuffle in zip(datasets,shuffles):
